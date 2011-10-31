5008f114a3b585efac5b16fc62a23c7d26c3758e
Juergen Falb <juergen@falb.at>
2011-10-31 11:33:11 +0100

    Updated DocuSign

---
+++ SignMe/Classes/RBFormLayoutData.h
@@ -0,0 +1,13 @@
+//
+//  RBFormLayoutData.h
+//  SignMe
+//
+//  Created by Juergen Falb on 22.10.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RBFormLayoutData : NSObject
+
+@end
---
+++ SignMe/Classes/RBFormLayoutData.m
@@ -0,0 +1,13 @@
+//
+//  RBFormLayoutData.m
+//  SignMe
+//
+//  Created by Juergen Falb on 22.10.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "RBFormLayoutData.h"
+
+@implementation RBFormLayoutData
+
+@end
